1c53497f2cf5f73bc5eaad634272ead039c8d257
Kofi <sambonful99@hotmail.com>
2022-04-20 22:11:31 +0100

    Rendered "Home" to HTML

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
 
 
 
+app.get("/", (req, res) => {
+  res.render("home", {startingContent: homeStartingContent})
+});
 
 
 
